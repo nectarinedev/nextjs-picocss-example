ee93fac5504ccf5f364b4df37733913373f5ad2d
chrissayon <christopher.l.sayon@gmail.com>
2022-11-09 00:55:58 +1100

    main: add signin page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,39 @@ export default function Home() {
     <div>
       <main>
         <article>
-          <h1>test</h1>
+          <h1>Sign In</h1>
+          <form>
+            <input
+              type='text'
+              name='login'
+              placeholder='Login'
+              aria-label='Login'
+              autoComplete='username'
+              required
+            />
+            <input
+              type='password'
+              name='password'
+              placeholder='Password'
+              aria-label='Password'
+              autoComplete='current-password'
+              required
+            />
+            <fieldset>
+              <label htmlFor='remember'>
+                <input
+                  type='checkbox'
+                  role='switch'
+                  id='remember'
+                  name='remember'
+                />
+                Remember me
+              </label>
+            </fieldset>
+            <button type='submit' className='contrast'>
+              Login
+            </button>
+          </form>
         </article>
       </main>
     </div>
